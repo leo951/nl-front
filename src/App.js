3d1c26113b7ea6d6a4cf8cf779075e9465a4eb6d
Léo Peyré <leo.peyre95@gmail.com>
2024-01-02 23:18:12 +0100

    create modal (no style)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { useEffect, useState } from "react";
+import styles from "./styles/App.module.scss";
 import MainLayout from "./components/layouts/MainLayout";
 import Home from "./components/home/Home";
+import LoginModal from "./components/modals/LoginModal";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, []);
+
+  useEffect(() => {
+    !token && setIsOpenModal(true);
+  }, [token]);
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
     <MainLayout>
       <Home />
+      {isOpenModal && <LoginModal closeModal={closeModal} />}
+      {isOpenModal && <div className={styles.overlay}></div>}
     </MainLayout>
   );
 }
---
+++ src/components/modals/LoginModal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./LoginModal.module.scss";
+
+const LoginModal = () => {
+  return <div className={styles.modal__container}>Testons la modal</div>;
+};
+
+export default LoginModal;
